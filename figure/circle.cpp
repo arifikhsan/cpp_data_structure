565ef5a2997d81747a8224942126c4ea7545c391
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-04-25 08:26:02 +0700

    tugas 2

---
+++ figure/circle.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+main()
+{
+  int radius = 7;
+
+  cout << "luas lingkaran = " << M_PI * pow(radius, 2) << endl;
+  cout << "keliling lingkaran = " << 2 * M_PI * radius << endl;
+}
---
+++ figure/square.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+main()
+{
+  int side = 10;
+
+  cout << "luas persegi = " << pow(10, 2) << endl;
+  cout << "keliling persegi = " << 10 * 4 << endl;
+}
