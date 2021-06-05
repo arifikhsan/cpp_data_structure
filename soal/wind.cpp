7804a13fd19a6feae2673e94a8f8b3af04675856
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-06-06 06:45:33 +0700

    wind

---
+++ soal/wind.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int windSpeed;
+    cout << "Enter a wind speed: ";
+    cin >> windSpeed;
+
+    if (windSpeed < 25)
+    {
+        cout << "Not a strong wind" << endl;
+    }
+    else if (windSpeed >= 25 && windSpeed <= 38)
+    {
+        cout << "String wind" << endl;
+    }
+    else if (windSpeed >= 39 && windSpeed <= 54)
+    {
+        cout << "Gale" << endl;
+    }
+    else if (windSpeed >= 55 && windSpeed <= 72)
+    {
+        cout << "Whole Gale" << endl;
+    }
+    else if (windSpeed > 72)
+    {
+        cout << "Hurricane" << endl;
+    }
+    else
+    {
+        cout << "Something went wrong" << endl;
+    }
+}
